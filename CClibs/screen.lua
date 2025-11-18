53096e87aa70cdf6344b2300740f432741081096
BreadDuck <bobopopcornboy8@gmail.com>
2025-11-18 13:52:38 -0800

    Refactor screen library functions and pixel handling

--- CClibs/screen.lua
+++ CClibs/screen.lua
@@ -1,18 +1,22 @@
 width,height = term.getSize()
 local pixels = {}
+local emptyPixels = {}
 local acceptedColorsK = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'}
 local acceptedColors = {}
+screenLib = {}
 for i,v in pairs(acceptedColorsK) do
     acceptedColors[v] = colors.fromBlit(v)
 end
 for x=1,width do
     pixels[x] = {}
+    emptyPixels[x] = {}
     for y=1,height do
         pixels[x][y] = "f"
+        emptyPixels[x][y] = "f"
     end
 end
 
-function getPixel(x,y)
+screenLib.getPixel = function(x,y)
     if x < 1 or x > width then
         return nil,1
     end
@@ -25,7 +29,7 @@ function getPixel(x,y)
     return nil,3
 end
 
-function setPixel(x,y,Rc)
+screenLib.setPixel = function(x,y,Rc)
     local c = tostring(Rc)
     c = c:lower()
     if not acceptedColors[c] then
@@ -44,17 +48,12 @@ function setPixel(x,y,Rc)
     return nil,3
 end
 
-function draw()
+screenLib.draw = function()
     for x=1, width do
         xROW = pixels[x]
         for y=1, height do
-            if math.mod(y,2) == 1 then
-                yITEM = xROW[y]
-                yITEMt = xROW[y+1]
-            else
-                yITEM = xROW[y-1]
-                yITEMt = xROW[y]
-            end
+            yITEM = xROW[y+1]
+            yITEMt = xROW[y]
             if not yITEM then
                 yITEM = 'f'
             end
@@ -65,7 +64,11 @@ function draw()
             term.setBackgroundColor(acceptedColors[yITEM])
             term.setTextColor(acceptedColors[yITEMt])
             term.setCursorPos(x,y)
-            term.write("\131")
+            if math.mod(y,2) == 0 or true then
+                term.write("\143")
+            else
+                term.write("\131")
+            end
         end
     end
 end
