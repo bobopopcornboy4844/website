32c32b2847c1bd4ffe53e6436c7849a34bcdd898
BreadDuck <bobopopcornboy8@gmail.com>
2025-11-25 14:39:11 -0800

    Refactor intersection logic and color calculation
    Refactor LineLineIntersection usage and improve color handling.

--- CCapps/doom.lua
+++ CCapps/doom.lua
@@ -8,6 +8,9 @@ function LineLineIntersection(X1,Y1,X2,Y2,X3,Y3,X4,Y4)
   local t_bottom = ((X1-X2)*(Y3-Y4)) - ((Y1-Y2)*(X3-X4))
   local t = t_top/t_bottom
   local u = u_top/t_bottom
+  if not (u and t) then
+    error('g')
+  end
   local ItX, ItY
   ItX = X1 + t*(X2-X1)
   ItY = Y1 + t*(Y2-Y1)
@@ -39,12 +42,18 @@ map = {
 
 function calcRoom(lDir,room,tLIST,uLIST)
   for i,v in pairs(room) do
-    local hitX,hitY,t,u = LineLineIntersection(x,y,x+math.sin(lDir),y+math.cos(lDir),  v[1],v[2],v[3],v[4])
-    if u > 0 and u < 1 then
-      tLIST[#tLIST+1] = t
-      uLIST[#uLIST+1] = u
-    end
+    local hitX,hitY,t,u = LineLineIntersection(x,y,x+math.cos(lDir),y+math.sin(lDir),  v[1],v[2],v[3],v[4])
+    tLIST[#tLIST+1] = t
+    uLIST[#tLIST] = u
   end
+end
+
+for i=1,16 do
+  local gray = i/16
+  if gray > 1 then
+    gray = 0
+  end
+  term.setPaletteColor(2^(i-1),gray,gray,gray)
 end
 
 function draw()
@@ -55,21 +64,31 @@ function draw()
     local tLIST = {}
     local uLIST = {}
     calcRoom(lDir,map[currentRoom],tLIST,uLIST)
-    lowest = 99999999999
-    lowestID = 1
-    for i,v in pairs(tLIST) do
-      if v < lowest and v > 0 then
-        lowest = v
-        lowestID = i
+    if #tLIST > 1 then
+      local lowest = 99999999999
+      local lowestID = 0
+      for i,v in pairs(tLIST) do
+        if v < lowest and v > 0 then
+          local u = uLIST[i]
+          if u > 0 and u < 1 then
+            lowest = v
+            lowestID = i
+          end
+        end
       end
-    end
-    file.writeLine(lowest)
-    lineHeight = lowest/2
-    color = uLIST[lowestID]
-    
+      if lowestID ~= 0 then
+        local lineHeight = lowest/2
+        local color = uLIST[lowestID]
+        color = color/(1/16)
+        color = math.floor(color)
+        color = 2 ^ color
 
-    for y=1,lineHeight do
-      screenLib.setPixel(x,y+(trueHeight/2)-(lineHeight/2),colors.toBlit(color))
+        file.writeLine(lowest..' '..color)
+
+        for y=1,lineHeight do
+          screenLib.setPixel(x,y+(trueHeight/2)-(lineHeight/2),colors.toBlit(color))
+        end
+      end
     end
   end
   file.close()
@@ -84,11 +103,12 @@ while true do
   draw()
   e,p1 = os.pullEvent()
   if e == 'key' then
-    debug = tostring(p1)
+    --debug = tostring(p1)
     if p1 == 262 then
       dir = dir + 1
     elseif p1 == 263 then
       dir = dir - 1
     end
+    debug = tostring(dir)
   end
 end
